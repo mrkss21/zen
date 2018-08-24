3eebe567a674c0f3c62f478c29091f1f90eb0136
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-24 11:24:23 +0300

    Update chal.sh

--- chal.sh
+++ chal.sh
@@ -3,5 +3,5 @@
 trkDir="~/nodetracker/"
 trkService="zentracker@$USER"
 if [[ $( cat ~/nodetracker/config/local/lastExecSec | cut -d. -f1 ) -ge 300 ]]; then
-        curl -s https://$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Stat check: connected to:)\S*").zensystem.io/$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Node t_address \(not for stake\)=)\w+")/$(cat ~$trkDir/config/config.json | grep -oP "(?<=\"nodeid\": )\d+")/send
+        curl -s https://$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Stat check: connected to:)\S*").zensystem.io/$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Node t_address \(not for stake\)=)\w+")/$(cat ~/nodetracker/config/config.json | grep -oP "(?<=\"nodeid\": )\d+")/send
 fi
