96544a88b0589dd78acba12726a5ae02fe365186
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-24 10:46:56 +0300

    Update chal-test.sh

--- chal-test.sh
+++ chal-test.sh
@@ -1,11 +1,4 @@
-	cat << EOF | sudo tee /home/znode/chal.sh
-#!/bin/bash
-trkDir="~/nodetracker/"
-trkService="zentracker@$USER"
-if [[ $( cat $trkDir/config/local/lastExecSec | cut -d. -f1 ) -ge 300 ]]; then
-        curl -s https://$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Stat check: connected to:)\S*").zensystem.io/$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Node t_address \(not for stake\)=)\w+")/$(cat ~$trkDir/config/config.json | grep -oP "(?<=\"nodeid\": )\d+")/send
-fi
-EOF
+wget https://raw.githubusercontent.com/mrkss21/zen/master/chal.sh
 chmod 777 chal.sh
 crontab -l > tempcron
 echo "*/15 * * * * /home/znode/chal.sh" >> tempcron
