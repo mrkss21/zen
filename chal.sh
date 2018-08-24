e5cd1cb7b549bea9a58018640fb507979672eab3
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-24 15:01:51 +0300

    Update chal.sh

--- chal.sh
+++ chal.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-trkDir="~/nodetracker/"
+trkDir="$HOME/nodetracker/"
 trkService="zentracker@$USER"
-if [[ $( cat ~/nodetracker/config/local/lastExecSec | cut -d. -f1 ) -ge 300 ]]; then
-        curl -s https://$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Stat check: connected to:)\S*").zensystem.io/$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Node t_address \(not for stake\)=)\w+")/$(cat ~/nodetracker/config/config.json | grep -oP "(?<=\"nodeid\": )\d+")/send
+waitForRestart="1m"
+maxAllowChallenge="300"
+
+function getUrlData(){
+  currSrv=$(journalctl -u $trkService --no-pager 2>/dev/null | tac  | grep -oP -m1 "(?<=Connected to server | Stat check: connected to: )\w*\.\w*")
+  currTa=$(journalctl -u $trkService --no-pager 2>/dev/null | tac | grep -oP -m1 "(?<=Node t_address \(not for stake\)=)\w+")
+  currNid=$(cat ~$trkDir/config/config.json | grep -oP "(?<=\"nodeid\": )\d+")}
+}
+
+if [[ $( cat $trkDir/config/local/lastExecSec | cut -d. -f1 ) -ge $maxAllowChallenge ]]; then
+  getUrlData  
+  if [[ -z "$currSrv" ]] || [[ -z "$currTa" ]] || [[ -z "$currNid" ]]; then
+    tkrPid=$(systemctl status $trkService | grep -oP "(?<=PID: )\d+")
+    kill $tkrPid
+    sleep $waitForRestart
+    getUrlData
+  fi
+        curl -s https://$currSrv.zensystem.io/$currTa/$currNid/send
 fi
