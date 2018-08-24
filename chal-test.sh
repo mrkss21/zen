3ecad1291166a5af283d2ef15277dd00622642b8
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-08-24 14:29:45 +0300

    Update chal-test.sh

--- chal-test.sh
+++ chal-test.sh
@@ -1,6 +1,6 @@
 wget https://raw.githubusercontent.com/mrkss21/zen/master/chal.sh
 chmod 777 chal.sh
 crontab -l > tempcron
-echo "*/15 * * * * /home/znode/chal.sh" >> tempcron
+echo "*/7 * * * * /home/znode/chal.sh" >> tempcron
 crontab tempcron
 rm tempcron
